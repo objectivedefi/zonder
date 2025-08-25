f9bb90006b07fe4923670116c44ee8ad3f8ce2e8
totomanov <totomanov@protonmail.com>
2025-08-25 12:56:00 +0300

    fix: expose event filters

--- src/lib/zonder/index.ts
+++ src/lib/zonder/index.ts
@@ -3,3 +3,4 @@ export * from './validateConfig.js';
 export * from './resolveStartBlock.js';
 export * from './zonder.js';
 export * from './resolveStartBlock.js';
+export * from './eventFilters.js';
